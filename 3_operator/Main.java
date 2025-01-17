eccae78de479d54bb6e169ecc07cc22217c85d72
Chhai Chivon <chivon.chhai.kh@gmail.com>
2025-01-17 18:54:41 +0700

    3_operator

---
+++ 3_operator/Main.java
@@ -0,0 +1,10 @@
+
+class Main{
+    public static void main(String[] args){
+        if(1==1){
+            System.out.println("1===1");
+        }else{
+            System.out.println("1!===1");
+        }
+    }
+}
---
+++ 3_operator/main.dart
@@ -0,0 +1,49 @@
+
+void main(){
+
+  // Arithmetic Operators
+  // int num1 = 3;
+  // int num2 = 2;
+  // print("Addition: ${num1 + num2}");
+  // print("Subtraction: ${num1 - num2}");
+  // print("Multiplication: ${num1 * num2}");
+  // print("Division: ${num1 / num2}");
+  // print("Modulus: ${num1 % num2}");
+
+
+
+  // Comparison Operators
+  // int num1 = 3;
+  // int num2 = 2;
+  // print("Equal: ${num1 == num2}");
+  // print("Not Equal: ${num1 != num2}");
+  // print("Greater Than: ${num1 > num2}");
+  // print("Less Than: ${num1 < num2}");
+
+
+  // Logical Operators
+  // bool isTrue = true, isFalse = false;
+  // print("AND: ${isTrue && isFalse}");
+  // print("OR: ${isTrue || isFalse}");
+  // print("NOT: ${!isTrue}");
+
+
+  // Bitwise Operators
+  // int num1 = 3; // 0011
+  // int num2 = 2; // 0010
+  // print("OR Operator: ${num1 | num2}"); // 0011 => 3
+  // print("AND Operator: ${num1 & num2}"); // 0010 => 2
+
+  // Assignment Operator
+  // dynamic num1 = 3; // Assign
+  // print(num1);
+  // print(num1 += 1); // Add and Assign
+  // print(num1 -= 1); // Subtract and Assign
+  // print(num1 *= 1); // Multiply and Assign
+  // print(num1 /= 2); // Divide and Assign
+
+  // Conditional Operator
+  bool isSingle = false;
+  print(isSingle ? "Single" : "Married");
+
+}
